a8bf16fa4c0060c65fab3b2d5db47f439eb7a87a
Doriszhaotutu <761725461@qq.com>
2017-01-11 17:37:19 +0800

    Update

--- binomial.py
+++ binomial.py
@@ -1,15 +1,24 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python
-"""
-Created on Tue Jan 10 14:56:53 2017
-
-@author: Administrator
-"""
+# Copyright (c) 2016 by Yuchao Zhao, Xiaoye Meng.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 from math import log,sqrt,exp
 from scipy import stats
 from os import sys, path
-from decimal import Decimal
-from decimal import getcontext
+
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 from brent import *
 
--- brent.py
+++ brent.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-import decimal
+
 
 def brent(a, b,  price, func, func2, func3, spot, strike, r,  d,  expiry, ssteps,tsteps):
     dd = None    
