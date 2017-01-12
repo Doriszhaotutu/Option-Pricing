d61060df32ef4c2a4555caaa3ec73dcc3dc83821
Doriszhaotutu <761725461@qq.com>
2017-01-12 14:54:47 +0800

    b issue
    Cost of carry（b）在计算不同的期权时需要设为不同的值。
    =r （无股息股票）
    = r-q（付股息的股票）
    =r-rf（外汇）
    =r-q+u（对商品，q为持有商品带来的便利收益或直接产生的收益 u为商品的储存成本）
    =0（对商品期货，可以把商品期货看做股息率=r的股票 即b = r -q=r-r =0）
    
    BTW 我写的BS公式中参数d表示股息率，计算无股息的股票期权时，应设为0。计算期货期权时，应设为r。对于期货期权b= r
    -d=0。BS中无参数b只有参数d。

--- baw.py
+++ baw.py
@@ -21,15 +21,15 @@ from os import sys, path
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 from bs import *
 
-def baw_call( spot,  strike,  r,  d,  vol,  expiry): 
+def baw_call( spot,  strike,  r,  b,  vol,  expiry): 
      M = 2.0 * r / (vol * vol)
-     N = 2.0 *  d / (vol * vol)
+     N = 2.0 *  b / (vol * vol)
      K = 1.0 - exp(-r * expiry)
      q2 = 0.5 * (-(N - 1) + sqrt((N - 1) * (N - 1) + 4 * M / K))
-     q2inf = 0.5 *(-(N - 1) + sqrt((N - 1) * (N - 1) + 4 * M))     
+     q2inf = 0.5 *(-(N - 1) + sqrt((N - 1) * (N - 1) + 4 * M))        
      Ssinf = strike / (1.0 - 1.0 / q2inf)
      stddev = vol * sqrt(expiry)
-     h2 = -( d * expiry + 2.0 * stddev) * (strike / (Ssinf - strike))
+     h2 = -( b * expiry + 2.0 * stddev) * (strike / (Ssinf - strike))
      S0 = strike + (Ssinf - strike) * (1.0 - exp(h2))
      g = 1.0
      gprime = 1.0
@@ -37,10 +37,10 @@ def baw_call( spot,  strike,  r,  d,  vol,  expiry):
      Si = S0
 
      while(abs(g) > 0.000001 and abs(gprime) > 0.000001 and niters < 500 and Si > 0.0): 
-        ce = bs_call(Si, strike, r, d, vol, expiry)
-        d1 = (log(Si / strike) + d * expiry + 0.5 * stddev * stddev) / stddev
-        g = (1.0 - 1.0 / q2) * Si - strike - ce + (1.0 / q2) * Si * exp(-( r - d ) * expiry) * stats.norm.cdf(d1, 0, 1)
-        gprime = (1.0 - 1.0 / q2) * (1.0 - exp(-(r - d) * expiry)) * stats.norm.cdf(d1, 0, 1) + (1.0 / q2) * exp(-(r - d ) * expiry) * stats.norm.cdf(d1, 0, 1) * (1.0 / stddev)
+        ce = bs_call(Si, strike, r, r - b, vol, expiry)
+        d1 = (log(Si / strike) + b * expiry + 0.5 * stddev * stddev) / stddev
+        g = (1.0 - 1.0 / q2) * Si - strike - ce + (1.0 / q2) * Si * exp(-( r - b ) * expiry) * stats.norm.cdf(d1, 0, 1)
+        gprime = (1.0 - 1.0 / q2) * (1.0 - exp(-(r - b) * expiry)) * stats.norm.cdf(d1, 0, 1) + (1.0 / q2) * exp(-(r - b ) * expiry) * stats.norm.cdf(d1, 0, 1) * (1.0 / stddev)
         Si -= g/gprime
         niters += 1
     
@@ -48,105 +48,105 @@ def baw_call( spot,  strike,  r,  d,  vol,  expiry):
         Ss = S0
      else:
         Ss = Si
-     ce = bs_call(spot, strike, r, d, vol, expiry)
+     ce = bs_call(spot, strike, r, r - b, vol, expiry)
      if (spot >= Ss):
         ca = spot - strike
      else: 
-        d1 = (log(Ss / strike) +  d * expiry + 0.5 * stddev * stddev) / stddev
-        A2 = (Ss / q2) * (1.0 - exp(-(r - d) * expiry) * stats.norm.cdf(d1, 0, 1))
+        d1 = (log(Ss / strike) +  b * expiry + 0.5 * stddev * stddev) / stddev
+        A2 = (Ss / q2) * (1.0 - exp(-(r - b) * expiry) * stats.norm.cdf(d1, 0, 1))
         ca = ce + A2 * pow(spot / Ss, q2)
 		
      return max(ca,ce)
 	
 	
 	# FIXME 
-def baw_put( spot,  strike,  r,  d,  vol,  expiry): 
+def baw_put( spot,  strike,  r,  b,  vol,  expiry): 
     M = 2.0 * r / (vol * vol)
-    N = 2.0 *  d / (vol * vol)
+    N = 2.0 *  b / (vol * vol)
     K = 1.0 - exp(-r * expiry)
     q1 = 0.5 * (-(N - 1) - sqrt((N - 1) * (N - 1) + 4 * M / K))
     q1inf = 0.5 *(-(N - 1) - sqrt((N - 1) * (N - 1) + 4 * M))
     Ssinf = strike / (1.0 - 1.0 / q1inf)
     stddev = vol * sqrt(expiry)
-    h1 = ( d * expiry - 2.0 * stddev) * (strike / (strike - Ssinf))
+    h1 = ( b * expiry - 2.0 * stddev) * (strike / (strike - Ssinf))
     S0 = Ssinf + (strike - Ssinf) * exp(h1)
     g = 1.0
     gprime = 1.0
     niters = 0
     Si = S0
     while (abs(g) > 0.000001 and abs(gprime) > 0.000001 and niters < 500 and Si > 0.0):
-         pe = bs_put(Si, strike, r, d, vol, expiry)
-         d1 = (log(Si / strike) +  d * expiry + 0.5 * stddev * stddev) / stddev
-         g = strike - Si - pe + (1.0 / q1) * Si * (1.0 - exp(-(r-d) * expiry)) * stats.norm.cdf(-d1, 0, 1)
-         gprime = (1.0 / q1 - 1.0) * (1.0 - exp(-(r-d) * expiry)) * stats.norm.cdf(-d1, 0, 1) + (1.0 / q1) * exp(-(r-d) * expiry) * stats.norm.cdf(-d1, 0, 1) * (1.0 / stddev)
+         pe = bs_put(Si, strike, r, r - b, vol, expiry)
+         d1 = (log(Si / strike) +  b * expiry + 0.5 * stddev * stddev) / stddev
+         g = strike - Si - pe + (1.0 / q1) * Si * (1.0 - exp(-(r-b) * expiry)) * stats.norm.cdf(-d1, 0, 1)
+         gprime = (1.0 / q1 - 1.0) * (1.0 - exp(-(r-b) * expiry)) * stats.norm.cdf(-d1, 0, 1) + (1.0 / q1) * exp(-(r-b) * expiry) * stats.norm.cdf(-d1, 0, 1) * (1.0 / stddev)
          Si -= g/gprime
          niters += 1
     if(abs(g) > 0.000001):
         Ss = S0
     else:
         Ss = Si
-    pe = bs_put(spot, strike, r, d, vol, expiry)
+    pe = bs_put(spot, strike, r, r - b, vol, expiry)
     if (spot <= Ss):
         pa = strike - spot
     else: 
-        d1 = (log(Ss / strike) +  d * expiry + 0.5 * stddev * stddev) / stddev
-        A1 = (-Ss / q1) * (1.0 - exp(-(r-d) * expiry) * stats.norm.cdf(-d1, 0, 1))
+        d1 = (log(Ss / strike) +  b * expiry + 0.5 * stddev * stddev) / stddev
+        A1 = (-Ss / q1) * (1.0 - exp(-(r-b) * expiry) * stats.norm.cdf(-d1, 0, 1))
         pa = pe + A1 * pow(spot / Ss, q1)
     return max(pa,pe)
 	
 	
 	# FIXME 
-def baw_call_delta( spot,  strike,  r,  d,  vol,  expiry): 
-    return (baw_call(1.001 * spot, strike, r, d, vol, expiry) - baw_call(0.999 * spot, strike, r, d, vol, expiry)) / (0.002 * spot)
+def baw_call_delta( spot,  strike,  r,  b,  vol,  expiry): 
+    return (baw_call(1.001 * spot, strike, r, b, vol, expiry) - baw_call(0.999 * spot, strike, r, b, vol, expiry)) / (0.002 * spot)
 	
 	
 	# FIXME 
-def baw_put_delta( spot,  strike,  r,  d,  vol,  expiry): 
-    return (baw_put(1.001 * spot, strike, r, d, vol, expiry) - baw_put(0.999 * spot, strike, r, d, vol, expiry)) / (0.002 * spot)
+def baw_put_delta( spot,  strike,  r,  b,  vol,  expiry): 
+    return (baw_put(1.001 * spot, strike, r, b, vol, expiry) - baw_put(0.999 * spot, strike, r, b, vol, expiry)) / (0.002 * spot)
 	
 	
 	# FIXME 
-def baw_call_gamma( spot,  strike,  r,  d,  vol,  expiry): 
-    return (baw_call_delta(1.01 * spot, strike, r, d, vol, expiry) - baw_call_delta(0.99 * spot, strike, r, d, vol, expiry)) / (0.02 * spot)
+def baw_call_gamma( spot,  strike,  r,  b,  vol,  expiry): 
+    return (baw_call_delta(1.01 * spot, strike, r, b, vol, expiry) - baw_call_delta(0.99 * spot, strike, r, b, vol, expiry)) / (0.02 * spot)
 	
 	
 	# FIXME 
-def baw_put_gamma( spot,  strike,  r,  d,  vol,  expiry): 
-    return (baw_put_delta(1.01 * spot, strike, r, d, vol, expiry) - baw_put_delta(0.99 * spot, strike, r, d, vol, expiry)) / (0.02 * spot)
+def baw_put_gamma( spot,  strike,  r,  b,  vol,  expiry): 
+    return (baw_put_delta(1.01 * spot, strike, r, b, vol, expiry) - baw_put_delta(0.99 * spot, strike, r, b, vol, expiry)) / (0.02 * spot)
 	
 	
 	# FIXME 
-def baw_call_theta( spot,  strike,  r,  d,  vol,  expiry): 
-    return (baw_call(spot, strike, r, d, vol, 1.01 * expiry) - baw_call(spot, strike, r, d, vol, 0.99 * expiry)) / (0.02 * expiry)
+def baw_call_theta( spot,  strike,  r,  b,  vol,  expiry): 
+    return (baw_call(spot, strike, r, b, vol, 1.01 * expiry) - baw_call(spot, strike, r, b, vol, 0.99 * expiry)) / (0.02 * expiry)
 	
 	
 	# FIXME 
-def baw_put_theta( spot,  strike,  r,  d,  vol,  expiry): 
-    return (baw_put(spot, strike, r, d, vol, 1.01 * expiry) - baw_put(spot, strike, r, d, vol, 0.99 * expiry)) / (0.02 * expiry)
+def baw_put_theta( spot,  strike,  r,  b,  vol,  expiry): 
+    return (baw_put(spot, strike, r, b, vol, 1.01 * expiry) - baw_put(spot, strike, r, b, vol, 0.99 * expiry)) / (0.02 * expiry)
 	
 	
 	# FIXME 
-def baw_call_vega( spot,  strike,  r,  d,  vol,  expiry): 
-    return (baw_call(spot, strike, r, d, 1.001 * vol, expiry) - baw_call(spot, strike, r, d, 0.999 * vol, expiry)) / (0.002 * vol)
+def baw_call_vega( spot,  strike,  r,  b,  vol,  expiry): 
+    return (baw_call(spot, strike, r, b, 1.001 * vol, expiry) - baw_call(spot, strike, r, b, 0.999 * vol, expiry)) / (0.002 * vol)
 	
 	
 	# FIXME 
-def baw_put_vega( spot,  strike,  r,  d,  vol,  expiry): 
-    return (baw_put(spot, strike, r, d, 1.001 * vol, expiry) - baw_put(spot, strike, r, d, 0.999 * vol, expiry)) / (0.002 * vol)
+def baw_put_vega( spot,  strike,  r,  b,  vol,  expiry): 
+    return (baw_put(spot, strike, r, b, 1.001 * vol, expiry) - baw_put(spot, strike, r, b, 0.999 * vol, expiry)) / (0.002 * vol)
 	
 	
 	# FIXME 
-def baw_call_rho( spot,  strike,  r,  d,  vol,  expiry): 
-    return (baw_call(spot, strike, 1.01 * r, d, vol, expiry) - baw_call(spot, strike, 0.99 * r, d, vol, expiry)) / (0.02 * r)
+def baw_call_rho( spot,  strike,  r,  b,  vol,  expiry): 
+    return (baw_call(spot, strike, 1.01 * r, b, vol, expiry) - baw_call(spot, strike, 0.99 * r, b, vol, expiry)) / (0.02 * r)
 	
 	
 	# FIXME 
-def baw_put_rho( spot,  strike,  r,  d,  vol,  expiry): 
-    return (baw_put(spot, strike, 1.01 * r, d, vol, expiry) -baw_put(spot, strike, 0.99 * r, d, vol, expiry)) / (0.02 * r)
+def baw_put_rho( spot,  strike,  r,  b,  vol,  expiry): 
+    return (baw_put(spot, strike, 1.01 * r, b, vol, expiry) -baw_put(spot, strike, 0.99 * r, b, vol, expiry)) / (0.02 * r)
 	
 	
 	# FIXME 
-def impv_baw( spot,  strike,  r,  d,  expiry,  price, optiontype): 
+def impv_baw( spot,  strike,  r,  b,  expiry,  price, optiontype): 
     low = 0.000001
     high = 0.3
 	
@@ -154,22 +154,22 @@ def impv_baw( spot,  strike,  r,  d,  expiry,  price, optiontype):
     if (optiontype != "AMER_CALL" and optiontype != "AMER_PUT"):
         return "NAN1"   
     if(optiontype == "AMER_CALL"):
-        ce = baw_call(spot, strike, r, d, high, expiry)
+        ce = baw_call(spot, strike, r, b, high, expiry)
     else:
-        ce = baw_put(spot, strike, r, d, high, expiry)
+        ce = baw_put(spot, strike, r, b, high, expiry)
     while (ce < price): 
         high *= 2.0
         if (high > 1e10):
             return "NAN2"
         if(optiontype == "AMER_CALL"):
-            ce = baw_call(spot, strike, r, d, high, expiry)
+            ce = baw_call(spot, strike, r, b, high, expiry)
         else:
-            ce = baw_put(spot, strike, r, d, high, expiry)
+            ce = baw_put(spot, strike, r, b, high, expiry)
 
     if(optiontype == "AMER_CALL"):	
-        return brent(low, high, price, baw_call, None, None, spot, strike, r, d, expiry, 0, 0)
+        return brent(low, high, price, baw_call, None, None, spot, strike, r, b, expiry, 0, 0)
     else:
-        return brent(low, high, price, baw_put,  None, None, spot, strike, r, d, expiry, 0, 0)
+        return brent(low, high, price, baw_put,  None, None, spot, strike, r, b, expiry, 0, 0)
         
 
 
